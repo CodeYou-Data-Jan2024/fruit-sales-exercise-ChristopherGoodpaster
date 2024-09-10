dad3e97c231d7848737b0c7d50866e1d0385ef6b
Christopher Goodpaster <chris.goodpaster83@gmail.com>
2024-09-10 19:32:15 -0400

    final goodpaster

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,15 @@
 # add your code here
+import pandas as pd
+
+# Create a DataFrame to match the table
+data = {"Apples": [35, 41],
+        "Bananas": [21, 34]}
+#create the index 
+index = ["2017 Sales", "2018 Sales"]
+
+df = pd.DataFrame(data, index=index)
+
+# Write the DataFrame to a CSV file
+df.to_csv("fruit.csv")
+
+print("Data written to fruit.csv")
